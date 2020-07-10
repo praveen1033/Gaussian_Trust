793842375ad08d30267edf299b1856431319bb3b
praveen1033 <m.praveentendulkar@hotmail.com>
2020-07-10 05:06:03 -0500

    Update Reading_Data.m

--- Reading_Data.m
+++ Reading_Data.m
@@ -1,4 +1,4 @@
 File_data = csvread('File6new.csv',1,1);                                      % Loads file to matrix called File_data
-I = unique(File_data(:,1));                                                             % I includes all smart meter ids.
-N = length(I);                                                                                 % N is the number of smart meters.
-Meter_Readings = File_data(:,3)*1000;                                       % Converts the usage in kilowatts to watts.
+I = unique(File_data(:,1));                                                   % I includes all smart meter ids.
+N = length(I);                                                                % N is the number of smart meters.
+Meter_Readings = File_data(:,3)*1000;                                         % Converts the usage in kilowatts to watts.
